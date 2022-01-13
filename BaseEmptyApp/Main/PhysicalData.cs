7de8a99910e10bca48e8ffe99f002634c32fd29f
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-01-13 09:47:21 +0300

    create 2 classes

---
+++ BaseEmptyApp/Main/PhysicalData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseEmptyApp.Main
+{
+    class PhysicalData
+    {
+        public double PhysicalAttack(int Strength, double Dexterity)
+        {
+            return Strength * 3 + 0.5 * Dexterity;
+        }
+        public double PhysicalDefense(double Constitution, int Dexterity)
+        {
+            return Constitution * 0.5 + Dexterity * 3;
+        }
+
+        public double MagicAttack(int Intelligence)
+        {
+            return Intelligence * 4;
+        }
+
+        public double MagicDefense(int Intelligence)
+        {
+            return Intelligence * 2;
+        }
+
+        public double PhysicalCriticalChanse(double Dexterity)
+        {
+            return 20 + Dexterity * 0.3;
+        }
+
+        public double MagicCriticalChanse(double Intelligence)
+        {
+            return 20 + Intelligence * 0.1;
+        }
+
+        public double PhysicalCriticalDamage(double PhysAttack, double Dexterity)
+        {
+            return PhysAttack * (2 + Dexterity * 0.05);
+        }
+
+        public double MagicCriticalDamage(double MagAttack, double Intelligence)
+        {
+            return MagAttack * (2 + Intelligence * 0.15);
+        }
+    }
+}
---
+++ BaseEmptyApp/Main/Сharacteristic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseEmptyApp.Main
+{
+    class Сharacteristic
+    {
+        public double Health(int Constitution, double Strength)
+        {
+            return 2 * Constitution + 0.5 * Strength;
+        }
+
+        public double Mana(int Intelligence)
+        {
+            return Intelligence * 3;
+        }
+    }
+}
