868e43d38a770c35c9bfce4385f0f3a0762b37e8
Ainur <ainur.hak2004@gmail.com>
2022-01-14 15:53:05 +0300

    Stats +- with exception

--- BaseEmptyApp/ChangeStats.xaml.cs
+++ BaseEmptyApp/ChangeStats.xaml.cs
@@ -32,42 +32,106 @@ namespace BaseEmptyApp
 
         private void btn_str_minus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Strength > CoreLibrary.Globals.character.minStr)
+            {
+                CoreLibrary.Globals.character.Strength--;
+                CoreLibrary.Globals.character.extraPoints++;
+            }
+            else MessageBox.Show($"Min value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_str.Content = $"Strength: {CoreLibrary.Globals.character.Strength}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_str_plus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Strength < CoreLibrary.Globals.character.maxStr)
+            {
+                CoreLibrary.Globals.character.Strength++;
+                CoreLibrary.Globals.character.extraPoints--;
+            }
+            else MessageBox.Show($"Max value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_str.Content = $"Strength: {CoreLibrary.Globals.character.Strength}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_dex_minus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Dexterity > CoreLibrary.Globals.character.minDex)
+            {
+                CoreLibrary.Globals.character.Dexterity--;
+                CoreLibrary.Globals.character.extraPoints++;
+            }
+            else MessageBox.Show($"Min value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_dex.Content = $"Dexterity: {CoreLibrary.Globals.character.Dexterity}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_dex_plus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Dexterity < CoreLibrary.Globals.character.maxDex)
+            {
+                CoreLibrary.Globals.character.Dexterity++;
+                CoreLibrary.Globals.character.extraPoints--;
+            }
+            else MessageBox.Show($"Max value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_dex.Content = $"Dexterity: {CoreLibrary.Globals.character.Dexterity}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_int_minus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Intelligence > CoreLibrary.Globals.character.minInt)
+            {
+                CoreLibrary.Globals.character.Intelligence--;
+                CoreLibrary.Globals.character.extraPoints++;
+            }
+            else MessageBox.Show($"Min value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_int.Content = $"Intelligence: {CoreLibrary.Globals.character.Intelligence}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
-
+    
         private void btn_int_plus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Intelligence < CoreLibrary.Globals.character.maxInt)
+            {
+                CoreLibrary.Globals.character.Intelligence++;
+                CoreLibrary.Globals.character.extraPoints--;
+            }
+            else MessageBox.Show($"Max value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_int.Content = $"Intelligence: {CoreLibrary.Globals.character.Intelligence}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_con_minus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Constitution > CoreLibrary.Globals.character.minCon)
+            {
+                CoreLibrary.Globals.character.Constitution--;
+                CoreLibrary.Globals.character.extraPoints++;
+            }
+            else MessageBox.Show($"Min value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_con.Content = $"Constitution: {CoreLibrary.Globals.character.Constitution}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_con_plus_Click(object sender, RoutedEventArgs e)
         {
+            if (CoreLibrary.Globals.character.Constitution < CoreLibrary.Globals.character.maxCon)
+            {
+                CoreLibrary.Globals.character.Constitution++;
+                CoreLibrary.Globals.character.extraPoints--;
+            }
+            else MessageBox.Show($"Max value", "error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+            lb_con.Content = $"Constitution: {CoreLibrary.Globals.character.Constitution}";
+            lb_extra_points.Content = $"Extra Points: {CoreLibrary.Globals.character.extraPoints}";
         }
 
         private void btn_back_Click(object sender, RoutedEventArgs e)
