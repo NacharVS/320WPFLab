6d6a5cfa0c321d16ded3d563fed894c7c7fc1d43
Егор Артемьев <batman_batman_1910@mail.ru>
2022-01-14 14:20:51 +0300

    create

---
+++ Core/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public abstract class Player
+{
+	public double strength = 0;
+	public double dexterity = 0;
+	public double intelligence = 0;
+	public double constitution = 0;
+
+	public Player(double str, double dex, double intell, double con) 
+	{
+		strength = str;
+		dexterity = dex;
+		intelligence = intell;
+		constitution = con;
+	}
+}
