2cfc523f6340967e3ae96e248fc3cc5d3feaca5b
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-01-14 16:34:55 +0300

    Пересчет статов после добавления/убавления поинтов

--- BaseEmptyApp/Core/Mage.cs
+++ BaseEmptyApp/Core/Mage.cs
@@ -30,5 +30,59 @@ namespace BaseEmptyApp.Core
                 return Strength;
             }
         }
+
+        public double Up_P_Attack(double str, double dex)
+        {
+            P_Attack = str * 3 + 0.5 * dex;
+            return P_Attack;
+        }
+
+        public double Up_P_Defense(double con, double dex)
+        {
+            P_Defense = con * 0.5 + dex * 3;
+            return P_Defense;
+        }
+
+        public double Up_M_Attack(double inT)
+        {
+            M_Defense = inT * 2;
+            return M_Defense;
+        }
+
+        public double Up_P_Critical_Chanse(double dex)
+        {
+            P_Critical_Chanse = 20 + dex * 0.3;
+            return P_Critical_Chanse;
+        }
+
+        public double Up_M_Critical_Chanse(double inT)
+        {
+            M_Critical_Chanse = 20 + inT * 0.1;
+            return M_Critical_Chanse;
+        }
+
+        public double Up_P_CriticalDamage(double p_att, double dex)
+        {
+            P_CriticalDamage = p_att * (2 + dex * 0.05);
+            return P_CriticalDamage;
+        }
+
+        public double Up_M_CriticalDamage(double m_att, double inT)
+        {
+            M_CriticalDamage = m_att * (2 + inT * 0.15);
+            return M_CriticalDamage;
+        }
+
+        public double Up_Health(double con, double str)
+        {
+            Health = 2 * con + 0.5 * str;
+            return Health;
+        }
+
+        public double Up_Mana(double inT)
+        {
+            Mana = inT * 3;
+            return Mana;
+        }
     }
 }
