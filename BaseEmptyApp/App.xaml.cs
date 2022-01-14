06b510396d7c8df4a8907588cbe77075ffc4224d
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2022-01-14 16:32:55 +0300

    added xaml frame

--- BaseEmptyApp/App.xaml.cs
+++ BaseEmptyApp/App.xaml.cs
@@ -10,20 +10,15 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BaseEmptyApp.Models;
 
 namespace BaseEmptyApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class App : Application
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+       
     }
 }
