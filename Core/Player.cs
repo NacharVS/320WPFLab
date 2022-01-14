91575b16e145b5cdcf9055f57b9c2e3fd45b35b0
Егор Артемьев <batman_batman_1910@mail.ru>
2022-01-14 14:37:09 +0300

    in process

--- Core/Player.cs
+++ Core/Player.cs
@@ -6,6 +6,8 @@ public abstract class Player
 	public double dexterity = 0;
 	public double intelligence = 0;
 	public double constitution = 0;
+	public double health = 0;
+	public double mana = 0;
 
 	public Player(double str, double dex, double intell, double con) 
 	{
@@ -13,5 +15,13 @@ public abstract class Player
 		dexterity = dex;
 		intelligence = intell;
 		constitution = con;
+	}
+	public static double Health(double con, double str) 
+	{
+		health = 2 * con + 0.5 * str;
+	}
+	public static double Mana(double intell) 
+	{
+		Mana = intell * 3;
 	}
 }
