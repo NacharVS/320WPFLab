f1bd343efa6dfbf4220a23b1c39405234cefbaeb
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2022-01-14 21:13:10 +0300

    added character properties view

--- BaseEmptyApp/MainWindow.xaml.cs
+++ BaseEmptyApp/MainWindow.xaml.cs
@@ -39,6 +39,30 @@ namespace BaseEmptyApp
             HealthTB.Text = "Health: " + warrior.Health.ToString();
         }
 
+        private void ChooseMageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Mage mage = new Mage();
+            NameTB.Text = mage.Name;
+            StrengthTB.Text = "Strength: " + mage.Strength.ToString();
+            DexterityTB.Text = "Dexterity: " + mage.Dexterity.ToString();
+            PAttackTB.Text = "P. Attack: " + mage.PAttack.ToString();
+            PResistTB.Text = "P.Resist: " + mage.PResist.ToString();
+            PhysCritChanceTB.Text = "Phys. Crit Chance: " + mage.PhysCritChance.ToString();
+            PhysCritDamageTB.Text = "Phys. Crit Damage: " + mage.PhysCritDamage.ToString();
+            HealthTB.Text = "Health: " + mage.Health.ToString();
+        }
 
+        private void ChooseArcherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Archer archer = new Archer();
+            NameTB.Text = archer.Name;
+            StrengthTB.Text = "Strength: " + archer.Strength.ToString();
+            DexterityTB.Text = "Dexterity: " + archer.Dexterity.ToString();
+            PAttackTB.Text = "P. Attack: " + archer.PAttack.ToString();
+            PResistTB.Text = "P.Resist: " + archer.PResist.ToString();
+            PhysCritChanceTB.Text = "Phys. Crit Chance: " + archer.PhysCritChance.ToString();
+            PhysCritDamageTB.Text = "Phys. Crit Damage: " + archer.PhysCritDamage.ToString();
+            HealthTB.Text = "Health: " + archer.Health.ToString();
+        }
     }
 }
