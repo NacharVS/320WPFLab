6c3dcd7660183fea01fad6efea33c289f059a532
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-01-17 17:25:33 +0300

    Добавила плюс\минус к статам

--- BaseEmptyApp/Core/Mage.cs
+++ BaseEmptyApp/Core/Mage.cs
@@ -18,11 +18,15 @@ namespace BaseEmptyApp.Core
         public Mage(double str, double dex, double inT, double con) : base(str, dex, inT, con)
         {
         }
+
         public override double Strength_Plus()
         {
             if (Strength < max_str)
             {
                 Strength = Strength + 1;
+                Up_P_Attack(Strength, Dexterity);
+                Up_P_CriticalDamage(P_Attack, Dexterity);
+                Up_Health(Constitution, Strength);
                 return Strength;
             }
             else
@@ -31,58 +35,121 @@ namespace BaseEmptyApp.Core
             }
         }
 
-        public double Up_P_Attack(double str, double dex)
+        public override double Strength_Minus()
         {
-            P_Attack = str * 3 + 0.5 * dex;
-            return P_Attack;
+            if (Strength < max_str)
+            {
+                Strength = Strength - 1;
+                Up_P_Attack(Strength, Dexterity);
+                Up_P_CriticalDamage(P_Attack, Dexterity);
+                Up_Health(Constitution, Strength);
+                return Strength;
+            }
+            else
+            {
+                return Strength;
+            }
         }
 
-        public double Up_P_Defense(double con, double dex)
+        public override double Dexterity_Plus()
         {
-            P_Defense = con * 0.5 + dex * 3;
-            return P_Defense;
+            if (Dexterity < max_dex)
+            {
+                Dexterity = Dexterity + 1;
+                Up_P_Attack(Strength, Dexterity);
+                Up_P_Defense(Constitution, Dexterity);
+                Up_P_Critical_Chanse(Dexterity);
+                Up_P_CriticalDamage(P_Attack, Dexterity);
+                return Dexterity;
+            }
+            else
+            {
+                return Dexterity;
+            }
         }
 
-        public double Up_M_Attack(double inT)
+        public override double Dexterity_Minus()
         {
-            M_Defense = inT * 2;
-            return M_Defense;
+            if (Dexterity < max_dex)
+            {
+                Dexterity = Dexterity - 1;
+                Up_P_Attack(Strength, Dexterity);
+                Up_P_Defense(Constitution, Dexterity);
+                Up_P_Critical_Chanse(Dexterity);
+                Up_P_CriticalDamage(P_Attack, Dexterity);
+                return Dexterity;
+            }
+            else
+            {
+                return Dexterity;
+            }
         }
 
-        public double Up_P_Critical_Chanse(double dex)
+        public override double Intelligence_Plus()
         {
-            P_Critical_Chanse = 20 + dex * 0.3;
-            return P_Critical_Chanse;
+            if (Intelligence < max_inT)
+            {
+                Intelligence = Intelligence + 1;
+                Up_M_Attack(Intelligence);
+                Up_M_Defense(Intelligence);
+                Up_M_Critical_Chanse(Intelligence);
+                Up_M_CriticalDamage(M_Attack, Intelligence);
+                Up_Mana(Intelligence);
+                return Intelligence;
+            }
+            else
+            {
+                return Intelligence;
+            }
         }
 
-        public double Up_M_Critical_Chanse(double inT)
+        public override double Intelligence_Minus()
         {
-            M_Critical_Chanse = 20 + inT * 0.1;
-            return M_Critical_Chanse;
+            if (Intelligence < max_inT)
+            {
+                Intelligence = Intelligence - 1;
+                Up_M_Attack(Intelligence);
+                Up_M_Defense(Intelligence);
+                Up_M_Critical_Chanse(Intelligence);
+                Up_M_CriticalDamage(M_Attack, Intelligence);
+                Up_Mana(Intelligence);
+                return Intelligence;
+            }
+            else
+            {
+                return Intelligence;
+            }
         }
 
-        public double Up_P_CriticalDamage(double p_att, double dex)
+        public override double Constitution_Plus()
         {
-            P_CriticalDamage = p_att * (2 + dex * 0.05);
-            return P_CriticalDamage;
+            if (Constitution < max_con)
+            {
+                Constitution = Constitution + 1;
+                Up_Health(Constitution, Strength);
+                Up_P_Defense(Constitution, Dexterity);
+                return Constitution;
+            }
+            else
+            {
+                return Constitution;
+            }
         }
 
-        public double Up_M_CriticalDamage(double m_att, double inT)
+        public override double Constitution_Minus()
         {
-            M_CriticalDamage = m_att * (2 + inT * 0.15);
-            return M_CriticalDamage;
+            if (Constitution < max_con)
+            {
+                Constitution = Constitution - 1;
+                Up_Health(Constitution, Strength);
+                Up_P_Defense(Constitution, Dexterity);
+                return Constitution;
+            }
+            else
+            {
+                return Constitution;
+            }
         }
 
-        public double Up_Health(double con, double str)
-        {
-            Health = 2 * con + 0.5 * str;
-            return Health;
-        }
-
-        public double Up_Mana(double inT)
-        {
-            Mana = inT * 3;
-            return Mana;
-        }
     }
 }
--- BaseEmptyApp/Core/Player.cs
+++ BaseEmptyApp/Core/Player.cs
@@ -41,6 +41,12 @@ namespace BaseEmptyApp
 
         public abstract double Strength_Plus();
         public abstract double Strength_Minus();
+        public abstract double Dexterity_Plus();
+        public abstract double Dexterity_Minus();
+        public abstract double Intelligence_Plus();
+        public abstract double Intelligence_Minus();
+        public abstract double Constitution_Plus();
+        public abstract double Constitution_Minus();
 
         public double NewChacarcter()
         {
@@ -87,5 +93,64 @@ namespace BaseEmptyApp
         //{
         //    return M_Attack(inT) * (2 + inT * 0.15);
         //}
+
+        public double Up_P_Attack(double str, double dex)
+        {
+            P_Attack = str * 3 + 0.5 * dex;
+            return P_Attack;
+        }
+
+        public double Up_P_Defense(double con, double dex)
+        {
+            P_Defense = con * 0.5 + dex * 3;
+            return P_Defense;
+        }
+
+        public double Up_M_Attack(double inT)
+        {
+            M_Attack = inT * 4;
+            return M_Defense;
+        }
+        public double Up_M_Defense(double inT)
+        {
+            M_Defense = inT * 2;
+            return M_Defense;
+        }
+
+        public double Up_P_Critical_Chanse(double dex)
+        {
+            P_Critical_Chanse = 20 + dex * 0.3;
+            return P_Critical_Chanse;
+        }
+
+        public double Up_M_Critical_Chanse(double inT)
+        {
+            M_Critical_Chanse = 20 + inT * 0.1;
+            return M_Critical_Chanse;
+        }
+
+        public double Up_P_CriticalDamage(double p_att, double dex)
+        {
+            P_CriticalDamage = p_att * (2 + dex * 0.05);
+            return P_CriticalDamage;
+        }
+
+        public double Up_M_CriticalDamage(double m_att, double inT)
+        {
+            M_CriticalDamage = m_att * (2 + inT * 0.15);
+            return M_CriticalDamage;
+        }
+
+        public double Up_Health(double con, double str)
+        {
+            Health = 2 * con + 0.5 * str;
+            return Health;
+        }
+
+        public double Up_Mana(double inT)
+        {
+            Mana = inT * 3;
+            return Mana;
+        }
     }
 }
