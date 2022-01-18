3fe57d40b6c7903504337177ae43f4f02f40b930
linar228 <linar369@mail.ru>
2022-01-18 12:51:08 +0300

    finish mainWindow

--- BaseEmptyApp/MainWindow.xaml.cs
+++ BaseEmptyApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +13,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Core;
 
 namespace BaseEmptyApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            ChangePage(new Warrior());
+        }
+
+        private void WarriorClick(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new Warrior());
+        }
+
+        private void MagicianClick(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new Magician());
+        }
+
+        private void ArcherClick(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new Archer());
+        }
+
+        private void ChangePage(Character character)
+        {
+            frameCharacter.NavigationService.Navigate(new CharacterPage(character));
         }
     }
 }
