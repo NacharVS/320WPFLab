091ecfc7613ba8fa8d6deb5c5db491ca9c799d63
Николаев Клементий <vapenationeron@gmail.com>
2022-01-18 05:03:48 +0300

    edit xaml

--- BaseEmptyApp/MainWindow.xaml.cs
+++ BaseEmptyApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BaseEmptyApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
