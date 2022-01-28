0b16b945b5d44c347c78f98a0db88d4868eba6ef
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-01-28 11:18:04 +0300

    Update project timer WPF

--- TimerWPF/MainWindow.xaml.cs
+++ TimerWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,62 @@ using System.Windows.Shapes;
 
 namespace TimerWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ConsistStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i <= Convert.ToInt32(timeSec.Text); i++)
+                {
+                    Thread.Sleep(1000);
+                    CheckLength(i);
+                }
+                MessageBox.Show("Время вышло!");
+            }
+            catch
+            {
+                MessageBox.Show("Не ввели число!");
+            }
+        }
+
+        private async void AsyncStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int time = Convert.ToInt32(timeSec.Text);
+                await Task.Run(() => TimeWait(time));
+                MessageBox.Show("Время вышло!");
+            }
+            catch
+            {
+                MessageBox.Show("Не ввели число!");
+            }
+        }
+
+        public void CheckLength(int i)
+        {
+            if (i.ToString().Length == 1)
+                lbTime.Content = $"00:00:0{i.ToString()}";
+            else if (i.ToString().Length == 2)
+                lbTime.Content = $"00:00:{i.ToString()}";
+        }
+
+        public void TimeWait(int time)
+        {
+            for (int i = 0; i <= time; i++)
+            {
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    CheckLength(i);
+                }));
+                Thread.Sleep(1000);
+            }
         }
     }
 }
